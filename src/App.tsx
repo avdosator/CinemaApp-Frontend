72b6e69a08ed41d76693273cfc7d2140578b7f6b
Avdo Sator <avdo.sator@hotmail.com>
2024-10-22 13:10:40 +0200

    Add some basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from './components/shared-components/movie-card/MovieCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       
       <h1>CinemaApp</h1>
+      <MovieCard/>
       
     </>
   )
--- src/components/shared-components/movie-card/MovieCard.tsx
+++ src/components/shared-components/movie-card/MovieCard.tsx
@@ -1,11 +1,13 @@
+import "./MovieCard.css"
+
 export default function MovieCard() {
     return (
         <>
             <div className="movie-card">
-                <img src="" alt="" className="movie-card-image" />
+                <img src="https://placehold.co/270x287" alt="" className="movie-card-image" />
                 <div>
                     <h5 className="movie-card-header">
-
+                        Avatar
                     </h5>
                     <div className="movie-card-details">
 

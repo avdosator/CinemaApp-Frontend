540194368c4528b94406c0edbdb728f991de473f
Avdo Sator <avdo.sator@hotmail.com>
2024-10-22 12:48:55 +0200

    Create MovieCard component

---
+++ src/components/shared-components/movie-card/MovieCard.tsx
@@ -0,0 +1,17 @@
+export default function MovieCard() {
+    return (
+        <>
+            <div className="movie-card">
+                <img src="" alt="" className="movie-card-image" />
+                <div>
+                    <h5 className="movie-card-header">
+
+                    </h5>
+                    <div className="movie-card-details">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

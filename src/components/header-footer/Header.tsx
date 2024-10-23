637a1c64b30e9f2835ef9c50372c7448632c4aa6
Avdo Sator <avdo.sator@hotmail.com>
2024-10-23 15:51:55 +0200

    Create skeleton for Header(navbar) component

---
+++ src/components/header-footer/Header.tsx
@@ -0,0 +1,24 @@
+export default function Header() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-content">
+                    <div className="navbar-logo">
+                        <div className="logo-container">
+                            <div className="logo-icon">
+                                <img src="/" alt="Cinebh Icon" />
+                            </div>
+                            <div className="logo-text">Cinebh.</div>
+                        </div>
+                    </div>
+                    <div className="navbar-links">
+
+                    </div>
+                    <div className="navbar-actions">
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}

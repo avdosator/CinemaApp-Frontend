9be96519c0c946ce29d2f41483640216ba104721
Avdo Sator <avdo.sator@hotmail.com>
2024-10-23 20:06:10 +0200

    Add styles to navbar

--- src/components/header-footer/Header.tsx
+++ src/components/header-footer/Header.tsx
@@ -1,3 +1,5 @@
+import "./Header.css"
+
 export default function Header() {
     return (
         <>
@@ -6,13 +8,15 @@ export default function Header() {
                     <div className="navbar-logo">
                         <div className="logo-container">
                             <div className="logo-icon">
-                                <img src="/" alt="Cinebh Icon" />
+                                <img src="https://placehold.co/26x24" alt="Cinebh Icon" />
                             </div>
                             <div className="logo-text">Cinebh.</div>
                         </div>
                     </div>
                     <div className="navbar-links">
-
+                        <a href="#" className="navbar-item active">Currently Showing</a>
+                        <a href="#" className="navbar-item">Upcoming Movies</a>
+                        <a href="#" className="navbar-item">Venues</a>
                     </div>
                     <div className="navbar-actions">
 

68fba21a4cc9b6df2dcb23a2fe315ebb00143c2e
Avdo Sator <avdo.sator@hotmail.com>
2024-10-25 23:15:24 +0200

    Add logic for slicing venue details if they are too long

--- src/components/shared-components/card/venue-card/VenueCard.tsx
+++ src/components/shared-components/card/venue-card/VenueCard.tsx
@@ -18,17 +18,21 @@ type VenueCardProps = {
 
 export default function VenueCard({ venue }: VenueCardProps) {
     const { id, name, street, streetNumber, city, phone } = venue;
-    return (
-        <>
-            <div className="shared-card">
-                <img src="https://placehold.co/270x287" alt="Venue Poster" className="shared-card-image" />
-                <div className="shared-card-content">
-                    <h5 className="shared-card-header">{name}</h5>
-                    <div className="shared-card-details">
-                        <div>{`${street} ${streetNumber}, ${city.name} ${city.postalCode}`}</div>
+    let details = `${street} ${streetNumber}, ${city.name} ${city.postalCode}`;
+    if (details.length + 4 >= 27) {
+        details = `${street} ${streetNumber}, ${city.name} ${city.postalCode}`.slice(0,30) + "...";
+    }
+        return (
+            <>
+                <div className="shared-card">
+                    <img src="https://placehold.co/270x287" alt="Venue Poster" className="shared-card-image" />
+                    <div className="shared-card-content">
+                        <h6 className="shared-card-header font-heading-h6">{name}</h6>
+                        <div className="shared-card-details">
+                            <div>{details}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
 }

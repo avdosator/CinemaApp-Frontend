4e658e82bd28f46e82db5df47648290367183c9a
Avdo Sator <avdo.sator@hotmail.com>
2024-10-28 18:21:49 +0100

    Implement get method that sends get requests

--- src/service/ApiService.ts
+++ src/service/ApiService.ts
@@ -8,4 +8,14 @@ export default class ApiService {
             "Content-Type": "application/json"
         }
     });
+
+    static async get<T>(route: string, headers: Record<string, string> = {}):  Promise<T> {
+        try {
+            const response = await this.axiosInstance.get(route, headers);
+            return response.data;
+        } catch(err) {
+            console.error("Bad request", err);
+            throw err;
+        }
+    }
 }

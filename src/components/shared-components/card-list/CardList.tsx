642fdaab8fba8e85eeab1ae380ace92f57762579
Avdo Sator <avdo.sator@hotmail.com>
2024-10-29 09:06:17 +0100

    Implement logic for pagination

--- src/components/shared-components/card-list/CardList.tsx
+++ src/components/shared-components/card-list/CardList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CardListProps } from "../../../types/CardListType"
 import "../../shared-components/card/SharedCard.css"
 import MovieCard from "../card/movie-card/MovieCard";
@@ -7,6 +8,24 @@ import "./CardList.css"
 
 export default function CardList({ elements }: CardListProps) {
     const { heading, movies, venues } = elements;
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4;
+    const items = movies || venues || [];
+    const totalItems = items.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const currentStart = (currentPage - 1) * itemsPerPage + 1;
+    const currentEnd = Math.min(currentPage * itemsPerPage, totalItems);
+    
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+    };
+
+    const displayedItems = items.slice(currentStart - 1, currentEnd);
     return (
         <>
             <div className="card-list-container">
@@ -16,19 +35,20 @@ export default function CardList({ elements }: CardListProps) {
                             <h2 className="font-heading-h4">{heading}</h2>
                             <a className="font-lg-semibold" href="">See All</a>
                         </div>
-                        <div className="card-list">
-                            {movies &&
-                                movies.map((movie) => (
-                                    <MovieCard key={movie.id} movie={movie} />
-                                ))}
-                            {venues &&
-                                venues.map((venue) => (
-                                    <VenueCard key={venue.id} venue={venue} />
-                                ))}
+                        <div className="card-list" >
+                            
                         </div>
                     </section>
                     <div className="pagination-sm-container">
-                        <PaginationSmall />
+                        <PaginationSmall
+                            currentStart={currentStart}
+                            currentEnd={currentEnd}
+                            totalItems={totalItems}
+                            onNext={handleNextPage}
+                            onPrev={handlePrevPage}
+                            hasNext={currentPage < totalPages}
+                            hasPrev={currentPage > 1}
+                        />
                     </div>
                 </div>
             </div>

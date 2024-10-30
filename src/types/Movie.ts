3fedd3a4b41fcfa1fea97a294733a1cf642554c1
Avdo Sator <avdo.sator@hotmail.com>
2024-10-30 09:46:46 +0100

    Edit movie type to properly handle API response

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,7 +1,18 @@
 export type Movie = {
     id: string,
     title: string,
-    duration: number,
-    genre: string
+    durationInMinutes: number,
+    genres: [
+        {
+            id: string,
+            name: string
+        }
+    ],
+    projections: [
+        {
+            id: "string",
+            status: "string"
+        }
+    ]
     // add other props
 }

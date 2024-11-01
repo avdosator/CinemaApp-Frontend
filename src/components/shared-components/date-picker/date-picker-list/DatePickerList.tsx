3fd149c2c8476ddfe2778e62cf6cf47500a608ed
Avdo Sator <avdo.sator@hotmail.com>
2024-11-01 11:15:59 +0100

    Create DatePickerList component

---
+++ src/components/shared-components/date-picker/date-picker-list/DatePickerList.tsx
@@ -0,0 +1,18 @@
+import "./DatePickerList.css"
+
+import DatePickerBtn from "../date-picker-btn/DatePickerBtn"
+
+export default function DatePickerList() {
+    // this array will represent todays date + 9 next dates (should be updated every day)
+    const array: { date: string, day: string }[] = [{ date: "Oct 31", day: "Today" }, { date: "Nov 1", day: "Fri" }, { date: "Nov 2", day: "Sat" }, { date: "Nov 3", day: "Sun" },
+    { date: "Nov 4", day: "Mon" }, { date: "Nov 5", day: "Tue" }, { date: "Nov 6", day: "Wed" }, { date: "Nov 7", day: "Tue" },
+    { date: "Nov 8", day: "Fri" }, { date: "Nov 9", day: "Sat" }
+    ]
+    return (
+        <div className="date-picker-list">
+            {array.map((item, index) => (
+                <DatePickerBtn  key={index} date={item.date} day={index === 0 ? "Today" : item.day} />
+            ))}
+        </div>
+    )
+}

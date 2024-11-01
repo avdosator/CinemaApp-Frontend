07086990d1fed7249cf480090013bdb6dfbde238
Avdo Sator <avdo.sator@hotmail.com>
2024-11-01 17:09:46 +0100

    Add page header

--- src/components/currently-showing-page/CurrentlyShowingPage.tsx
+++ src/components/currently-showing-page/CurrentlyShowingPage.tsx
@@ -5,6 +5,7 @@ import MovieCardBigList from "./movie-card-big-list/MovieCardBigList";
 export default function CurrentlyShowingPage() {
     return(
         <>  
+            <h4 className="font-heading-h4 currently-showing-caption">Currently showing(9)</h4>
             <DatePickerList />
             <div className="font-md-italic-regular date-reminder">
                 Quick reminder that our cinema schedule is on a ten-day update cycle.

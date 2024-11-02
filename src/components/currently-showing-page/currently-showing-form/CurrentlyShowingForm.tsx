cce59cc75e3ed74e4961cb51ab105bff1148e377
Avdo Sator <avdo.sator@hotmail.com>
2024-11-02 11:15:47 +0100

    Add style for created form

--- src/components/currently-showing-page/currently-showing-form/CurrentlyShowingForm.tsx
+++ src/components/currently-showing-page/currently-showing-form/CurrentlyShowingForm.tsx
@@ -1,48 +1,40 @@
+import "./CurrentlyShowingForm.css"
 import DatePickerList from "../../shared-components/date-picker/date-picker-list/DatePickerList";
 
 export default function CurrentlyShowingForm() {
     return (
         <div className="form-container">
             <div className="currently-showing-form">
-                <form>
-                    <div>
-                        <label htmlFor="">Search Movies</label>
-                        <input type="text" />
+                <form className="font-lg-regular">
+                    <div className="search-movies">
+                        <input type="text" className="search-movies-input font-lg-regular form-element" placeholder="Search Movies" />
                     </div>
-                    <div>
-                        <div>
-                            <select name="city" id="city">
-                                <option value="">All cities</option>
-                                <option value="Sarajevo">Sarajevo</option>
-                                <option value="Mostar">Mostar</option>
-                            </select>
-                        </div>
-                        <div>
-                            <select name="venue" id="venue">
-                                <option value="">All Cinemas</option>
-                                <option value="Cineplexx">Cineplexx</option>
-                                <option value="Cinestar">Cinestar</option>
-                            </select>
-                        </div>
-                        <div>
-                            <select name="genre" id="genre">
-                                <option value="">All Genres</option>
-                                <option value="Action">Action</option>
-                                <option value="War">War</option>
-                                <option value="Drama">Drama</option>
-                                <option value="Thriller">Thriller</option>
-                            </select>
-                        </div>
-                        <div>
-                            <select name="projectionTime" id="projectionTime">
-                                <option value="">All Projection Times</option>
-                                <option value="14:00">14:00</option>
-                                <option value="16:00">16:00</option>
-                                <option value="18:00">18:00</option>
-                                <option value="20:00">20:00</option>
-                                <option value="22:00">22:00</option>
-                            </select>
-                        </div>
+                    <div className="dropdown-menu-inputs">
+                        <select name="city" id="city" className="dropdown-menu-input form-element">
+                            <option value="">All cities</option>
+                            <option value="Sarajevo">Sarajevo</option>
+                            <option value="Mostar">Mostar</option>
+                        </select>
+                        <select name="venue" id="venue" className="dropdown-menu-input form-element">
+                            <option value="">All Cinemas</option>
+                            <option value="Cineplexx">Cineplexx</option>
+                            <option value="Cinestar">Cinestar</option>
+                        </select>
+                        <select name="genre" id="genre" className="dropdown-menu-input form-element">
+                            <option value="">All Genres</option>
+                            <option value="Action">Action</option>
+                            <option value="War">War</option>
+                            <option value="Drama">Drama</option>
+                            <option value="Thriller">Thriller</option>
+                        </select>
+                        <select name="projectionTime" id="projectionTime" className="dropdown-menu-input form-element">
+                            <option value="">All Projection Times</option>
+                            <option value="14:00">14:00</option>
+                            <option value="16:00">16:00</option>
+                            <option value="18:00">18:00</option>
+                            <option value="20:00">20:00</option>
+                            <option value="22:00">22:00</option>
+                        </select>
                     </div>
                 </form>
             </div>

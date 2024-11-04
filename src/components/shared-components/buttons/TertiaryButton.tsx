ce57b4b22cdb49f2dab8116a27e412e080ea4462
Avdo Sator <avdo.sator@hotmail.com>
2024-11-04 21:55:39 +0100

    Hide Load more button when there is no more content to show

--- src/components/shared-components/buttons/TertiaryButton.tsx
+++ src/components/shared-components/buttons/TertiaryButton.tsx
@@ -2,12 +2,13 @@ import "./TertiaryButton.css"
 
 type TertiaryButtonProps = {
     label: string,
-    size: string
+    size: string,
+    onClick?: () => void
 }
 
-export default function TertiaryButton({label, size}: TertiaryButtonProps) {
+export default function TertiaryButton({label, size, onClick}: TertiaryButtonProps) {
     return (
-        <button className={size === "large" ? "tertiary-btn-lg font-lg-underline-semibold" : ""}>
+        <button className={size === "large" ? "tertiary-btn-lg font-lg-underline-semibold" : ""} onClick={onClick}>
             {label}
         </button>
     )

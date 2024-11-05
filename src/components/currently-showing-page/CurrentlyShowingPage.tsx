b8bf864af38cf4ba16b37f553a64cc20ef0d26ca
Avdo Sator <avdo.sator@hotmail.com>
2024-11-05 15:41:51 +0100

    Make CardList component accept route string in props

--- src/components/currently-showing-page/CurrentlyShowingPage.tsx
+++ src/components/currently-showing-page/CurrentlyShowingPage.tsx
@@ -147,7 +147,7 @@ export default function CurrentlyShowingPage() {
 
     useEffect(() => {
         // On title change call fetchMovies after 500ms
-        debouncedFetchMovies(formData);
+        debouncedFetchMovies(formData);  
     }, [formData.title]);
 
     // Clean up debounced function on unmount
@@ -192,6 +192,8 @@ export default function CurrentlyShowingPage() {
                 genreOptions={genreOptions}
                 venueOptions={venueOptions}
                 timeOptions={projectionTimeOptions} />
+            <h4 className="font-heading-h4 currently-showing-caption">Currently showing{movies.length !== 0 ? `(${movies.length})` : ""}</h4>
+            <CurrentlyShowingForm handleChange={handleChange} handleDateChange={handleDateChange} formData={formData} />
             <div className="font-md-italic-regular date-reminder">
                 Quick reminder that our cinema schedule is on a ten-day update cycle.
             </div>

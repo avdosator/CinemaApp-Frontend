718e42e7bd756b4f711d27b0c5c9c08e879ed619
Avdo Sator <avdo.sator@hotmail.com>
2024-11-06 13:25:49 +0100

    Add state from CurrentlyShowingForm and implement getting form data from url if params are present

--- src/components/currently-showing-page/CurrentlyShowingPage.tsx
+++ src/components/currently-showing-page/CurrentlyShowingPage.tsx
@@ -11,26 +11,108 @@ import { PageResponse } from "../../types/PageResponse";
 import { CurrentlyShowingFormData } from "../../types/CurrentlyShowingFormData";
 import debounce from "lodash.debounce";
 import NoMoviesPreview from "../shared-components/no-movies-preview/NoMoviesPreview";
-import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import { City } from "../../types/City";
+import { Genre } from "../../types/Genre";
+import { Venue } from "../../types/Venue";
+import { calculateDateString } from "../../utils/utils";
 
 export default function CurrentlyShowingPage() {
-    const navigate = useNavigate();
+    let [searchParams, setSearchParams] = useSearchParams();
     let [movies, setMovies] = useState<Movie[]>([]);
-    let [formData, setFormData] = useState<CurrentlyShowingFormData>({ title: "", city: null, venue: null, genre: null, time: null, date: new Date().toISOString().split('T')[0] });
     let [page, setPage] = useState(0); // Current page number
     let [isLastPage, setIsLastPage] = useState(false); // Track if we're on the last page
     const PAGE_SIZE: number = 9;
+    const START_DATE: string = calculateDateString(0);
+    const END_DATE: string = calculateDateString(9);
+
+    let [cityOptions, setCityOptions] = useState<SelectOptionType[]>();
+    let [genreOptions, setGenreOptions] = useState<SelectOptionType[]>();
+    let [venueOptions, setVenueOptions] = useState<SelectOptionType[]>();
+    let [projectionTimeOptions, setProjectionTimeOptions] = useState<SelectOptionType[]>();
+
+    // set initial form state from url params
+    let [formData, setFormData] = useState<CurrentlyShowingFormData>({
+        title: searchParams.get("title") || "",
+        city: searchParams.get("city") ? { value: searchParams.get("city")!, label: "" } : null,
+        venue: searchParams.get("venue") ? { value: searchParams.get("venue")!, label: "" } : null,
+        genre: searchParams.get("genre") ? { value: searchParams.get("genre")!, label: "" } : null,
+        time: searchParams.get("time") ? { value: searchParams.get("time")!, label: "" } : null,
+        date: searchParams.get("date") || new Date().toISOString().split('T')[0]
+    });
+
+    useEffect(() => {
+        Promise.all([
+            ApiService.get<City[]>("/cities"),
+            ApiService.get<PageResponse<Venue>>("/venues"),
+            ApiService.get<Genre[]>("/genres"),
+            ApiService.get<string[]>("projections/start-times")
+        ])
+            .then(([citiesResponse, venuesResponse, genresResponse, projectionsTimesResponse]) => {
+
+                // If form data got initial state from url param(s) then find it and set its label
+                const cityOptions = citiesResponse.map(city => ({ value: city.id, label: city.name }));
+                setCityOptions(cityOptions);
+
+                if (formData.city?.value) {
+                    const cityOption = cityOptions.find(city => city.value === formData.city!.value);
+                    if (cityOption) setFormData(prev => ({ ...prev, city: cityOption }));
+                }
+
+                const venueOptions = venuesResponse.content.map(venue => ({ value: venue.id, label: venue.name }));
+                setVenueOptions(venueOptions);
+
+                if (formData.venue?.value) {
+                    const venueOption = venueOptions.find(venue => venue.value === formData.venue!.value);
+                    if (venueOption) setFormData(prev => ({ ...prev, venue: venueOption }));
+                }
+
+                const genreOptions = genresResponse.map(genre => ({ value: genre.id, label: genre.name }))
+                setGenreOptions(genreOptions);
+
+                if (formData.genre?.value) {
+                    const genreOption = genreOptions.find(genre => genre.value === formData.genre!.value);
+                    if (genreOption) setFormData(prev => ({ ...prev, genre: genreOption }));
+                }
+
+                const projectionTimeOptions = projectionsTimesResponse.map(projectionTime => ({ value: projectionTime, label: projectionTime }))
+                setProjectionTimeOptions(projectionTimeOptions);
+
+                if (formData.time?.value) {
+                    const projectionOption = projectionTimeOptions.find(projection => projection.value === formData.time!.value);
+                    if (projectionOption) setFormData(prev => ({ ...prev, time: projectionOption }));
+                }
+
+                setProjectionTimeOptions(projectionsTimesResponse.map(projection => ({ value: projection, label: projection })));
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    }, []);
+
+    const updateSearchParams = (data: CurrentlyShowingFormData, pageNumber: number) => {
+
+        const params: Record<string, string> = {
+            page: pageNumber.toString(),
+            size: PAGE_SIZE.toString(),
+            START_DATE,
+            END_DATE
+        };
+        if (data.title) params.title = data.title;
+        if (data.city?.value) params.city = data.city.value;
+        if (data.venue?.value) params.venue = data.venue.value;
+        if (data.genre?.value) params.genre = data.genre.value;
+        if (data.time?.value) params.time = data.time.value;
+        if (data.date) params.date = data.date;
+
+        setSearchParams(params);
+    };
 
     const fetchMovies = (data: CurrentlyShowingFormData, pageNumber: number) => {
-        const today = new Date();
-        const startDate = today.toISOString().split('T')[0];
-        const endDate = new Date(today.setDate(today.getDate() + 9)).toISOString().split('T')[0];
 
         const params: Record<string, string | undefined | number> = {
             page: pageNumber,
             size: PAGE_SIZE,
-            startDate: startDate,
-            endDate: endDate,
+            startDate: START_DATE,
+            endDate: END_DATE,
             city: data.city?.value,
             venue: data.venue?.value,
             genre: data.genre?.value,
@@ -40,15 +122,6 @@ export default function CurrentlyShowingPage() {
         if (data.title) {
             params.title = data.title;
         }
-
-        // Convert params object to URL search params for ApiService and URL update
-        const urlParams = new URLSearchParams();
-        Object.entries(params).forEach(([key, value]) => {
-            if (value !== undefined) urlParams.append(key, value.toString());
-        });
-
-        // Update the URL in the address bar
-        navigate(`/currently-showing?${urlParams.toString()}`, { replace: true });
 
         ApiService.get<PageResponse<Movie>>("/movies", params)
             .then(response => {
@@ -61,14 +134,19 @@ export default function CurrentlyShowingPage() {
     }
 
     const debouncedFetchMovies = debounce((data: CurrentlyShowingFormData) => {
-        // Reset to the first page when search parameters change
         setPage(0);
-        fetchMovies(data, 0);
+        // Update formData for title changes
+        setFormData(prevData => ({ ...prevData, title: data.title }));
+        // Update searchParams with the new title value
+        updateSearchParams({ ...formData, title: data.title }, 0);
+        // Fetch movies with the updated title
+        fetchMovies({ ...formData, title: data.title }, 0);
     }, 500);
 
     useEffect(() => {
         // Fetch immediately when non-title fields change
         setPage(0);
+        updateSearchParams(formData, 0);
         fetchMovies(formData, 0);
     }, [formData.city, formData.venue, formData.genre, formData.time, formData.date, page]);
 
@@ -111,7 +189,14 @@ export default function CurrentlyShowingPage() {
     return (
         <>
             <h4 className="font-heading-h4 currently-showing-caption">Currently showing{movies.length !== 0 ? `(${movies.length})` : "(0)"}</h4>
-            <CurrentlyShowingForm handleChange={handleChange} handleDateChange={handleDateChange} formData={formData} />
+            <CurrentlyShowingForm
+                handleChange={handleChange}
+                handleDateChange={handleDateChange}
+                formData={formData}
+                cityOptions={cityOptions}
+                genreOptions={genreOptions}
+                venueOptions={venueOptions}
+                timeOptions={projectionTimeOptions} />
             <div className="font-md-italic-regular date-reminder">
                 Quick reminder that our cinema schedule is on a ten-day update cycle.
             </div>

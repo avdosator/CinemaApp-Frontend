f426b3eb39c4bf364a1dcf9c40fda47d16da675d
Avdo Sator <avdo.sator@hotmail.com>
2024-11-06 13:24:40 +0100

    Delete state for select options from this component

--- src/components/currently-showing-page/currently-showing-form/CurrentlyShowingForm.tsx
+++ src/components/currently-showing-page/currently-showing-form/CurrentlyShowingForm.tsx
@@ -3,52 +3,28 @@ import DatePickerList from "../../shared-components/date-picker/date-picker-list
 import Select, { SingleValue } from "react-select";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faLocationPin, faClock } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from "react";
 import { SelectOptionType } from "../../../types/SelectOptionType";
-import ApiService from "../../../service/ApiService";
-import { PageResponse } from "../../../types/PageResponse";
-import { City } from "../../../types/City";
-import { Genre } from "../../../types/Genre";
-import { Venue } from "../../../types/Venue";
 import { CurrentlyShowingFormData } from "../../../types/CurrentlyShowingFormData";
 
 type CurrentlyShowingFormProps = {
     handleChange: (name: string, value: string | SingleValue<SelectOptionType>) => void,
     handleDateChange: (date: string) => void,
-    formData: CurrentlyShowingFormData
+    formData: CurrentlyShowingFormData,
+    cityOptions?: SelectOptionType[]; // Marked as optional with '?'
+    genreOptions?: SelectOptionType[];
+    venueOptions?: SelectOptionType[];
+    timeOptions?: SelectOptionType[];
 }
 
-export default function CurrentlyShowingForm({handleChange, handleDateChange, formData}: CurrentlyShowingFormProps) {
-    
-    let [cityOptions, setCityOptions] = useState<SelectOptionType[]>();
-    let [genreOptions, setGenreOptions] = useState<SelectOptionType[]>();
-    let [venueOptions, setVenueOptions] = useState<SelectOptionType[]>();
-    let [projectionOptions, setProjectionOptions] = useState<SelectOptionType[]>();
+export default function CurrentlyShowingForm({
+    handleChange,
+    handleDateChange,
+    formData,
+    cityOptions,
+    genreOptions,
+    venueOptions,
+    timeOptions }: CurrentlyShowingFormProps) {
 
-    useEffect(() => {
-        Promise.all([
-            ApiService.get<City[]>("/cities"),
-            ApiService.get<PageResponse<Venue>>("/venues"),
-            ApiService.get<Genre[]>("/genres"),
-            ApiService.get<string[]>("projections/start-times")
-        ])
-        .then(([citiesResponse, venuesResponse, genresResponse, projectionsResponse]) => {
-            const cities = citiesResponse || [];
-            setCityOptions(cities.map(city => ({ value: city.id, label: city.name })));
-    
-            const venues = venuesResponse.content || [];
-            setVenueOptions(venues.map(venue => ({ value: venue.id, label: venue.name })));
-    
-            const genres = genresResponse || [];
-            setGenreOptions(genres.map(genre => ({ value: genre.id, label: genre.name })));
-
-            const projectionTimes = projectionsResponse || []; 
-            setProjectionOptions(projectionTimes.map(projection => ({value: projection, label: projection})));
-        })
-        .catch(error => console.error("Error fetching data:", error));
-    }, []);
-
-    
     return (
         <div className="form-container">
             <div className="currently-showing-form">
@@ -106,7 +82,7 @@ export default function CurrentlyShowingForm({handleChange, handleDateChange, fo
                         <div className="input-wrapper">
                             <FontAwesomeIcon icon={faClock} className="input-icon" />
                             <Select<SelectOptionType, false>
-                                options={projectionOptions}
+                                options={timeOptions}
                                 placeholder="All Projection Times"
                                 className="dropdown-menu-input"
                                 classNamePrefix="dropdown"

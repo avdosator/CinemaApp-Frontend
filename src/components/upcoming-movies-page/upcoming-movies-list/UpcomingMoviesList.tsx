cc4bd1d298594ec4797f1836aa66b7a7834f7219
Avdo Sator <avdo.sator@hotmail.com>
2024-11-09 16:19:41 +0100

    Implement some responsive design for CurrentlyShowing/UpcomingMovies forms

--- src/components/upcoming-movies-page/upcoming-movies-list/UpcomingMoviesList.tsx
+++ src/components/upcoming-movies-page/upcoming-movies-list/UpcomingMoviesList.tsx
@@ -5,7 +5,7 @@ import MovieCard from "../../shared-components/card/movie-card/MovieCard";
 export default function UpcomingMoviesList({movies}: {movies: Movie[]}) {
     return (
         <div className="upcoming-movies-list">
-            {movies.map((movie, index) => (
+            {movies.map((movie) => (
                 <MovieCard key={movie.id} {...movie} />
             ))}
             {/* Dummy elements that move last MovieCard(s) to the left */}

177960333aa6c0e4a6c8ce5ebf5b7515be2b441a
Avdo Sator <avdo.sator@hotmail.com>
2024-11-11 20:14:16 +0100

    Create basic MovieDetailsPage structure

---
+++ src/components/movie-details-page/MovieDetailsPage.tsx
@@ -0,0 +1,29 @@
+export default function MovieDetailsPage() {
+    return (
+        <div>
+            <h1>Movie Details</h1>
+            <section className="video-photo-container">
+                <div className="video-container">
+
+                </div>
+                <div className="movie-photos-container">
+
+                </div>
+            </section>
+            <section className="movie-info-ticket-container">
+                <div className="movie-info-container">
+
+                </div>
+                <div className="movie-ticket-container">
+
+                </div>
+            </section>
+            <section className="movie-rating-container">
+
+            </section>
+            <section className="movie-card-small-list">
+
+            </section>
+        </div>
+    )
+}

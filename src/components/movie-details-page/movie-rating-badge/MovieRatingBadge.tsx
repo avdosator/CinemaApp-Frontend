0766fc19bc2fec96faf3f61f8291e291f6d03764
Avdo Sator <avdo.sator@hotmail.com>
2024-11-11 21:37:59 +0100

    Create component that shows movie ratings from imdb and rt

---
+++ src/components/movie-details-page/movie-rating-badge/MovieRatingBadge.tsx
@@ -0,0 +1,16 @@
+import { faStar as faStarRegular } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function MovieRatingBadge({ rating, label }: { rating: number, label: string }) {
+    return (
+        <div className="movie-rating-badge">
+            <div className="star-icon">
+            <FontAwesomeIcon icon={faStarRegular} />
+            </div>
+            <div className="movie-rating-info">
+                <div className="font-md-semibold" style={{ color: "#101828" }}>rating</div>
+                <div className="font-sm-regular" style={{ color: "#667085" }}>label</div>
+            </div>
+        </div>
+    )
+}

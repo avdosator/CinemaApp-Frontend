d45924079632431146ce0b1d988677e68dd2422e
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 09:24:17 +0100

    Create css file for styling MovieCardSmall

--- src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
+++ src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
@@ -1,7 +1,7 @@
 export default function MovieCardSmall() {
     return (
         <div className="movie-card-small">
-            <img src="#" alt="" />
+            <img src="#" alt="" className="movie-card-small-img" />
             <h5 className="font-lg-semibold" style={{color: "#1D2939"}}>Avatar</h5>
         </div>
     )

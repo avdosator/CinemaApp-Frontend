cf07d83a7e8f6f46aba6a7e72baa4187d225e363
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 15:40:14 +0100

    Add logic for small pagination and fetching movies from API

--- src/components/movie-details-page/movie-card-small-list/MovieCardSmallList.tsx
+++ src/components/movie-details-page/movie-card-small-list/MovieCardSmallList.tsx
@@ -3,25 +3,51 @@ import MovieCardSmall from "../../shared-components/card/movie-card-small/MovieC
 import { Movie } from "../../../types/Movie";
 import ApiService from "../../../service/ApiService";
 import { PageResponse } from "../../../types/PageResponse";
+import PaginationSmall from "../../shared-components/pagination/PaginationSmall";
 
 export default function MovieCardSmallList() {
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 6;
+    const totalItems = movies.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const currentStart = totalItems > 0 ? (currentPage - 1) * itemsPerPage + 1 : 0;
+    const currentEnd = Math.min(currentPage * itemsPerPage, totalItems);
+    let lastPageStyle = currentEnd === totalItems && (totalItems % 4) > 0 ? { justifyContent: "start", gap: "10px" } : {};
 
     useEffect(() => {
         ApiService.get<PageResponse<Movie>>("/movies/active")
             .then(response => setMovies(response.content))
             .catch(error => console.error("Error fetching data:", error));
     }, []);
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+    };
+
+    const displayedItems = movies.slice(currentStart - 1, currentEnd);
+
     return (
         <div className="movie-card-small-container">
             <h5 className="font-heading-h6" style={{ color: "#1D2939" }}>See also</h5>
             <div className="movie-card-small-list">
-                <MovieCardSmall />
-                <MovieCardSmall />
-                <MovieCardSmall />
-                <MovieCardSmall />
+                {displayedItems.map(item => (
+                    (<MovieCardSmall />)
+                ))}
             </div>
-            {/* <PaginationSmall /> */}
+            <PaginationSmall
+                currentStart={currentStart}
+                currentEnd={currentEnd}
+                totalItems={totalItems}
+                onNext={handleNextPage}
+                onPrev={handlePrevPage}
+                hasNext={currentPage < totalPages}
+                hasPrev={currentPage > 1}
+            />
         </div>
     )
 }

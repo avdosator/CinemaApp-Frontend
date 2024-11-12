b60bbea5bc5b3ed440ed3203429c2521366b74fb
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 13:41:14 +0100

    Create reusable VerticalDivider component

---
+++ src/components/shared-components/divider/VerticalDivider.tsx
@@ -0,0 +1,13 @@
+type VerticalDividerProps = {
+    width?: string,
+    color?: string,
+    border?: string,
+}
+
+
+export default function VerticalDivider({ width = "20px", color = "#B22222", border = "1px solid #B22222" }: VerticalDividerProps) {
+    const style = { width: width, color: color, border: border};
+    return (
+        <div className="vertical-divider" style={style}></div>
+    )
+}

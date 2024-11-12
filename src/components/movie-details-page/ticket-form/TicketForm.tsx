a977264291e2851440aac05d746c82f1d8006b40
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 10:09:18 +0100

    Add more elements and styles to TicketForm

--- src/components/movie-details-page/ticket-form/TicketForm.tsx
+++ src/components/movie-details-page/ticket-form/TicketForm.tsx
@@ -1,3 +1,5 @@
+import TimeBadge from "../../shared-components/time-badge/TimeBadge";
+
 export default function TicketForm() {
     return (
         <div className="ticket-form-container">
@@ -8,11 +10,13 @@ export default function TicketForm() {
 
             </div>
             <div className="movie-projecton-times">
-
+                <h6 className="font-heading-h6" style={{ color: "#1D2939" }}>Standard</h6>
+                <TimeBadge label="16:00" isSelected={false} onClick={() => { }} />
+                <TimeBadge label="18:00" isSelected={false} onClick={() => { }} />
             </div>
-            <div>
-                <button>Reserve Ticket</button>
-                <button>Buy Ticket</button>
+            <div className="ticket-btns font-lg-semibold">
+                <button className="ticket-btn-reserve">Reserve Ticket</button>
+                <button className="ticket-btn-buy">Buy Ticket</button>
             </div>
         </div>
     )

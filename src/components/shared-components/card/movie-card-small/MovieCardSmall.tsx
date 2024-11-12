cd2521cd148d3efc3cc7f6a2dbfd5d6dddd8062d
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 09:10:26 +0100

    Create MovieCardSmall component

---
+++ src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
@@ -0,0 +1,8 @@
+export default function MovieCardSmall() {
+    return (
+        <div className="movie-card-small">
+            <img src="#" alt="" />
+            <h5 className="font-lg-semibold" style={{color: "#1D2939"}}>Avatar</h5>
+        </div>
+    )
+}

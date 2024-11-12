d937e0b36da1d13499b5ddadb3263ca0e0d3ee29
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 09:40:30 +0100

    Cretate TicketForm element

--- src/components/movie-details-page/MovieDetailsPage.tsx
+++ src/components/movie-details-page/MovieDetailsPage.tsx
@@ -1,9 +1,10 @@
 import PaginationSmall from "../shared-components/pagination/PaginationSmall";
+import TicketForm from "./ticket-form/TicketForm";
 
 export default function MovieDetailsPage() {
     return (
         <div>
-            <h1>Movie Details</h1>
+            <h5 className="font-heading-h5" style={{ color: "#1D2939" }}>Movie Details</h5>
             <section className="video-photo-container">
                 <div className="video-container">
 
@@ -14,8 +15,8 @@ export default function MovieDetailsPage() {
             </section>
             <section className="movie-info-ticket-container">
                 <div className="movie-info-container">
-                    <h2>Avatar</h2>
-                    <div>
+                    <h4 className="font-heading-h4" style={{ color: "#1D2939" }}>Avatar</h4>
+                    <div className="font-lg-regular" style={{ color: "#1D2939" }}>
                         <span>PG rating</span>
                         <span>language</span>
                         <span>duration</span>
@@ -24,30 +25,28 @@ export default function MovieDetailsPage() {
                     <div>
                         genres.map(genre = (GenreBadge) )
                     </div>
-                    <p className="movie-synopsis font-lg-regular" style={{color: "#1D2939"}}>Synopsis</p>
+                    <p className="movie-synopsis font-lg-regular" style={{ color: "#1D2939" }}>Synopsis</p>
                     <div className="movie-director">
                         Director
                     </div>
                     <div className="writers-container font-lg-regular">
-                        <span style={{color: "667085"}}>Writers:</span>
-                        <span style={{ color: "#1D2939"}}> James Cameron, Cameron James</span>
+                        <span style={{ color: "667085" }}>Writers:</span>
+                        <span style={{ color: "#1D2939" }}> James Cameron, Cameron James</span>
                     </div>
                     <div className="actors-container">
-                        <h6 className="font-heading-h6" style={{color: "#667085"}}>Cast</h6>
-                        <div>Actors.map</div>
+                        <h6 className="font-heading-h6" style={{ color: "#667085" }}>Cast</h6>
+                        <div>Actors.map(div actor)</div>
                     </div>
                 </div>
-                <div className="movie-ticket-container">
-
-                </div>
+                <TicketForm />
             </section>
             <section className="movie-rating-container">
-                <h6 className="font-heading-h6" style={{color: "#667085"}}>Rating</h6>
+                <h6 className="font-heading-h6" style={{ color: "#667085" }}>Rating</h6>
                 <div>imdb rating badge</div>
                 <div>rt rating badge</div>
             </section>
             <section className="movie-card-small-list">
-                <h5 className="font-heading-h6" style={{color: "#1D2939"}}>See also</h5>
+                <h5 className="font-heading-h6" style={{ color: "#1D2939" }}>See also</h5>
                 <div>cardlist</div>
                 <PaginationSmall />
             </section>
---
+++ src/components/movie-details-page/ticket-form/TicketForm.tsx
@@ -0,0 +1,19 @@
+export default function TicketForm() {
+    return (
+        <div className="ticket-form-container">
+            <div className="movie-select-container">
+
+            </div>
+            <div className="date-picker-btn-container">
+
+            </div>
+            <div className="movie-projecton-times">
+
+            </div>
+            <div>
+                <button>Reserve Ticket</button>
+                <button>Buy Ticket</button>
+            </div>
+        </div>
+    )
+}

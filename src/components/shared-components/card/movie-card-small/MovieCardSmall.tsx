613449c7a980e4d695b3b5e22d561edeeaadbcdf
Avdo Sator <avdo.sator@hotmail.com>
2024-11-12 22:59:04 +0100

    Send movie as props to every MovieCardSmall

--- src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
+++ src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import { Movie } from "../../../../types/Movie";
 
-export default function MovieCardSmall({movie}: {movie: Movie}) {
+export default function MovieCardSmall({ movie }: { movie: Movie }) {
     const coverPhoto = movie.photos.find(photo => photo.id === movie.coverPhotoId);
     return (
-        <div className="movie-card-small">
-            <img src={coverPhoto?.url} alt="" className="movie-card-small-img" />
-            <h5 className="font-lg-semibold" style={{color: "#1D2939"}}>{movie.title}</h5>
-        </div>
+        <Link to={`/movies/${movie.id}`}>
+            <div className="movie-card-small">
+                <img src={coverPhoto?.url} alt="" className="movie-card-small-img" />
+                <h5 className="font-lg-semibold" style={{ color: "#1D2939" }}>{movie.title}</h5>
+            </div>
+        </Link>
     )
 }

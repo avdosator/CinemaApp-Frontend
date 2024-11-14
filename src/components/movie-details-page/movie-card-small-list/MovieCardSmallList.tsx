527179902e1928352f30a983283132615979a7c9
Avdo Sator <avdo.sator@hotmail.com>
2024-11-14 10:25:12 +0100

    Style MovieCardSmallList and cut off title if it is too long to fit in the card

--- src/components/movie-details-page/movie-card-small-list/MovieCardSmallList.tsx
+++ src/components/movie-details-page/movie-card-small-list/MovieCardSmallList.tsx
@@ -13,7 +13,7 @@ export default function MovieCardSmallList() {
     const totalPages = Math.ceil(totalItems / itemsPerPage);
     const currentStart = totalItems > 0 ? (currentPage - 1) * itemsPerPage + 1 : 0;
     const currentEnd = Math.min(currentPage * itemsPerPage, totalItems);
-    let lastPageStyle = currentEnd === totalItems && (totalItems % 4) > 0 ? { justifyContent: "start", gap: "10px" } : {};
+    let lastPageStyle = currentEnd === totalItems && (totalItems % 4) > 0 ? { justifyContent: "start", gap: "16px" } : {};
 
     const handleNextPage = () => {
         if (currentPage < totalPages) setCurrentPage(currentPage + 1);
@@ -31,15 +31,17 @@ export default function MovieCardSmallList() {
             <div className="movie-card-small-list" style={lastPageStyle}>
                 {displayedItems.map(item => (<MovieCardSmall key={item.id} movie={item} />))}
             </div>
-            <PaginationSmall
-                currentStart={currentStart}
-                currentEnd={currentEnd}
-                totalItems={totalItems}
-                onNext={handleNextPage}
-                onPrev={handlePrevPage}
-                hasNext={currentPage < totalPages}
-                hasPrev={currentPage > 1}
-            />
+            <div className="pagination-sm-container">
+                <PaginationSmall
+                    currentStart={currentStart}
+                    currentEnd={currentEnd}
+                    totalItems={totalItems}
+                    onNext={handleNextPage}
+                    onPrev={handlePrevPage}
+                    hasNext={currentPage < totalPages}
+                    hasPrev={currentPage > 1}
+                />
+            </div>
         </div>
     )
 }
--- src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
+++ src/components/shared-components/card/movie-card-small/MovieCardSmall.tsx
@@ -4,11 +4,13 @@ import { Movie } from "../../../../types/Movie";
 
 export default function MovieCardSmall({ movie }: { movie: Movie }) {
     const coverPhoto = movie.photos.find(photo => photo.id === movie.coverPhotoId);
+    const title = movie.title.length > 25 ? movie.title.slice(0, 22) + '...' : movie.title;
+    
     return (
         <Link to={`/movies/${movie.id}`} className="no-style-link">
             <div className="movie-card-small">
                 <img src={coverPhoto?.url} alt="" className="movie-card-small-img" />
-                <h5 className="font-lg-semibold" style={{ color: "#1D2939" }}>{movie.title}</h5>
+                <h5 className="font-lg-semibold" style={{ color: "#1D2939" }}>{title}</h5>
             </div>
         </Link>
     )

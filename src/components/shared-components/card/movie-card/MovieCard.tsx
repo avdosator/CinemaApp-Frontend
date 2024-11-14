9e064fc2230d5539ac445870492832ab2d6197d0
Avdo Sator <avdo.sator@hotmail.com>
2024-11-14 15:12:10 +0100

    Make some improvements on images

--- src/components/shared-components/card/movie-card/MovieCard.tsx
+++ src/components/shared-components/card/movie-card/MovieCard.tsx
@@ -12,7 +12,9 @@ export default function MovieCard({ id, title, durationInMinutes, genres, photos
     return (
         <Link to={`/movies/${id}`} className="no-style-link">
             <div className="shared-card">
-                <img src={photos[0].url} alt="Movie Poster" className="shared-card-image" />
+                <div className="shared-card-img-container">
+                    <img src={photos[0].url} alt="Movie Poster" className="shared-card-image" />
+                </div>
                 <div className="shared-card-content">
                     <h6 className="shared-card-header font-heading-h6">{title}</h6>
                     <div className="shared-card-details font-md-regular">

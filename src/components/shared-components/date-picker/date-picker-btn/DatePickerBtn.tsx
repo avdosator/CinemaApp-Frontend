9db39774a083ad789fe74f2bc9b7b242ed381c06
Avdo Sator <avdo.sator@hotmail.com>
2024-11-15 16:02:07 +0100

    Edit classname

--- src/components/shared-components/date-picker/date-picker-btn/DatePickerBtn.tsx
+++ src/components/shared-components/date-picker/date-picker-btn/DatePickerBtn.tsx
@@ -11,7 +11,7 @@ type DatePickerBtnProps = {
 export default function DatePickerBtn({size, date, day, pickDate, selected}: DatePickerBtnProps) {
     // it should take prop something like size= "small" or "big", so we can reuse it in movie details page where it is smaller 
     return(
-        <button className={`date-picker-btn-${size} ${selected ? "selected" : ""}`} onClick={pickDate}>
+        <button className={`date-picker-btn-${size} ${selected ? "selected-date-btn" : ""}`} onClick={pickDate}>
             <div className={`btn-date ${selected ? "font-heading-h6" : "font-lg-semibold"}`} >{date}</div>
             <div className="font-lg-regular btn-day">{day}</div>
         </button>

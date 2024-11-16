812f45f6749bd0193a7a3e15e581be629b1988c2
Avdo Sator <avdo.sator@hotmail.com>
2024-11-16 10:17:11 +0100

    Add select elements for city and cinema to form

--- src/components/movie-details-page/ticket-form/TicketForm.tsx
+++ src/components/movie-details-page/ticket-form/TicketForm.tsx
@@ -1,17 +1,55 @@
 import "./TicketForm.css"
 import TimeBadge from "../../shared-components/time-badge/TimeBadge";
 import { Movie } from "../../../types/Movie";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding, faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import Select from "react-select";
+import { SelectOptionType } from "../../../types/SelectOptionType";
+import DatePickerBtnSmall from "../../shared-components/date-picker/date-picker-btn-small/DatePickerBtnSmall";
 
 export default function TicketForm({ movie }: { movie: Movie }) {
+    //const [selectedOption, setSelectedOption] = useState<SelectOptionType | null>();
+
+    movie.projections[0]
+    const cityOptions: SelectOptionType[] = [{ value: "1", label: "Sarajevo" }, { value: "2", label: "Mostar" }];
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+    }
+
     return (
-        <form className="ticket-form-container">
+        <form className="ticket-form-container" onSubmit={handleSubmit}>
             <div>
-                <div className="movie-select-container">
-                    <select name="" id=""></select>
-                    <select name="" id=""></select>
+                <div className="dropdown-menu-inputs ticket-inputs">
+                    <div className="input-wrapper">
+                        <FontAwesomeIcon icon={faLocationPin} className="input-icon" />
+                        <Select<SelectOptionType, false>
+                            options={cityOptions}
+                            placeholder="Choose city"
+                            className="dropdown-menu-input"
+                            classNamePrefix="dropdown"
+                            isClearable={true}
+                            value={null}
+                            onChange={(newvalue) => console.log(newvalue)}
+                            name="city"
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <FontAwesomeIcon icon={faBuilding} className="input-icon" />
+                        <Select<SelectOptionType, false>
+                            options={cityOptions}
+                            placeholder="Choose Cinema"
+                            className="dropdown-menu-input"
+                            classNamePrefix="dropdown"
+                            isClearable={true}
+                            value={null}
+                            onChange={(newvalue) => console.log(newvalue)}
+                            name="city"
+                        />
+                    </div>
                 </div>
                 <div className="date-picker-btn-container">
-
+                    <DatePickerBtnSmall date={"Dec 22"} day={"tue"} selected={false} />
                 </div>
             </div>
             <div className="movie-projecton-times">
@@ -23,8 +61,6 @@ export default function TicketForm({ movie }: { movie: Movie }) {
                         ))
                     })}
                 </div>
-                {/* <TimeBadge label="16:00" isSelected={false} onClick={() => { }} />
-                <TimeBadge label="18:00" isSelected={false} onClick={() => { }} /> */}
             </div>
             <div className="ticket-btns-container">
                 <div className="horizontal-line"></div>

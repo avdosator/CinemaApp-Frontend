7162153f3bc5a33d1e70948e09215c35730eb4f5
Avdo Sator <avdo.sator@hotmail.com>
2024-11-26 12:54:43 +0100

    Check if password and confirm password match

--- src/components/auth/sign-up/SignUpForm.tsx
+++ src/components/auth/sign-up/SignUpForm.tsx
@@ -9,7 +9,7 @@ type SignupFormType = {
 }
 
 export default function SignUpForm() {
-    const { register, handleSubmit, formState: { errors } } = useForm<SignupFormType>();
+    const { register, handleSubmit, formState: { errors }, watch } = useForm<SignupFormType>();
 
     const onSubmit: SubmitHandler<SignupFormType> = (formData) => {
         console.log(formData);
@@ -30,10 +30,10 @@ export default function SignUpForm() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="input-icon" fill="#344054" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z" /></svg>
                         <input
                             {...register("email", {
-                                required: "Enter your email address",
+                                required: "Email address is required.",
                                 pattern: {
                                     value: /^(?!(^[.-].*|[^@]*\.@|.*\.{2,}.*)|^.{254}.)([a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+@)(?!-.*|.*-\.)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,15}$/,
-                                    message: "Invalid email address"
+                                    message: "Invalid email address."
                                 }
                             })}
                             type="text"
@@ -51,11 +51,13 @@ export default function SignUpForm() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="input-icon" fill="#344054" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z" /></svg>
                         <input
                             {...register("password", {
-                                required: "Password is required",
+                                required: "Password is required.",
                                 minLength: {
                                     value: 8,
-                                    message: "Password must be at least 8 characters long"
-                                }
+                                    message: "Password must be at least 8 characters long."
+                                },
+                                validate: (value) =>
+                                    value === watch("confirmPassword") || "Passwords do not match."
                             })}
                             type="password"
                             name="password"
@@ -76,8 +78,10 @@ export default function SignUpForm() {
                                 required: "Password is required",
                                 minLength: {
                                     value: 8,
-                                    message: "Password must be at least 8 characters long"
-                                }
+                                    message: "Password must be at least 8 characters long."
+                                },
+                                validate: (value) =>
+                                    value === watch("password") || "Passwords do not match."
                             })}
                             type="password"
                             name="confirmPassword"

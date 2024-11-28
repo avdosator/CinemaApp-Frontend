3e6af40ae0fdd40c2a5fd0b44a44ea047b2aa803
Avdo Sator <avdo.sator@hotmail.com>
2024-11-28 22:13:32 +0100

    Extract PasswordToggleIcon to separate component

--- src/components/auth/sign-up/SignUpForm.tsx
+++ src/components/auth/sign-up/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import "../AuthForm.css"
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
+import PasswordToggleIcon from "../password-reset/PasswordToggleIcon";
 
 type SignupFormType = {
     email: string,
@@ -8,7 +9,13 @@ type SignupFormType = {
     confirmPassword: string
 }
 
-export default function SignUpForm({ closeAuthContainer }: { closeAuthContainer: () => void }) {
+type SignUpFormProps = {
+    closeAuthContainer: () => void,
+    backToSignIn: () => void
+
+}
+
+export default function SignUpForm({ closeAuthContainer, backToSignIn }: SignUpFormProps) {
     const { register, handleSubmit, setError, clearErrors, formState: { errors, isSubmitting }, watch, trigger } = useForm<SignupFormType>();
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
@@ -66,7 +73,7 @@ export default function SignUpForm({ closeAuthContainer }: { closeAuthContainer:
     return (
         <div className="auth-form-container">
             <div className="auth-container-heading">
-                <button className="auth-back-btn" onClick={closeAuthContainer}>
+                <button className="auth-back-btn" onClick={backToSignIn}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#D0D5DD" width="18" height="24" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" /></svg>
                 </button>
                 <h5 className="font-heading-h5 auth-heading">Hello</h5>
@@ -112,19 +119,11 @@ export default function SignUpForm({ closeAuthContainer }: { closeAuthContainer:
                             className="font-lg-regular"
                             placeholder="Password"
                         />
-                        <svg
+                        <PasswordToggleIcon
                             onClick={toggleShowPassword}
-                            className={`show-password-icon ${errors.password && passwordValue ? "error-icon" : ""}`}
-                            fill={
-                                errors.password && passwordValue
-                                    ? "#FDA29B" // Error color if there's an error and value exists
-                                    : !passwordValue
-                                        ? "#667085" // Default color when input is empty
-                                        : "#344054" // Default color for populated field
-                            }
-                            viewBox="0 0 640 512"
-                        >
-                            <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm9.4 130.3C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5l-41.9-33zM192 256c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5z" /></svg>
+                            error={!!errors.password && !!passwordValue}
+                            isVisible={showPassword}
+                        />
                     </div>
                     {errors.password && <div className="font-sm-regular auth-error">{errors.password.message}</div>}
                 </div>
@@ -147,19 +146,11 @@ export default function SignUpForm({ closeAuthContainer }: { closeAuthContainer:
                             className="font-lg-regular"
                             placeholder="Retype Password"
                         />
-                        <svg
+                        <PasswordToggleIcon
                             onClick={toggleShowConfirmPassword}
-                            className={`show-password-icon ${errors.confirmPassword && confirmPasswordValue ? "error-icon" : ""}`}
-                            fill={
-                                errors.confirmPassword && confirmPasswordValue
-                                    ? "#FDA29B" // Error color if there's an error and value exists
-                                    : !passwordValue
-                                        ? "#667085" // Default color when input is empty
-                                        : "#344054" // Default color for populated field
-                            }
-                            viewBox="0 0 640 512"
-                        >
-                            <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm9.4 130.3C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5l-41.9-33zM192 256c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5z" /></svg>
+                            error={!!errors.confirmPassword && !!confirmPasswordValue}
+                            isVisible={showPassword}
+                        />
                     </div>
                     {errors.confirmPassword && <div className="font-sm-regular auth-error">{errors.confirmPassword.message}</div>}
                 </div>

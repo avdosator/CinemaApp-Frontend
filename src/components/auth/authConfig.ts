d01ed97bc078de5b69542da3cc0ed96f1ccbaf07
Avdo Sator <avdo.sator@hotmail.com>
2024-12-01 20:34:33 +0100

    Add options for AuthSuccess element

--- src/components/auth/authConfig.ts
+++ src/components/auth/authConfig.ts
@@ -1,19 +1,27 @@
-export const authStepHeadings: Record<"signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword", string> = {
+export const authStepHeadings: Record<
+    "signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword" | "successfulSignIn" | "successfulSignUp" | "successfulPasswordChange",
+    string> = {
     signIn: "Welcome Back",
     signUp: "Hello",
     passwordResetEmail: "Reset Password",
     passwordResetCode: "Reset Password",
-    newPassword: "Reset Password"
+    newPassword: "Reset Password",
+    successfulSignIn: "Sign In Successful! 🎉",
+    successfulSignUp: "You’re all set! 🎉",
+    successfulPasswordChange: "Password Reset Successful! 🎉"
 };
 
 // action for back button in AuthContainer / AuthHeading
 export const backActions = (
     closeAuthContainer: () => void,
-    setAuthStep: (step: "signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword") => void
-): Record<"signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword", () => void> => ({
+    setAuthStep: (step: "signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword" | "successfulSignIn" | "successfulSignUp" | "successfulPasswordChange",) => void
+): Record<"signIn" | "signUp" | "passwordResetEmail" | "passwordResetCode" | "newPassword" | "successfulSignIn" | "successfulSignUp" | "successfulPasswordChange", () => void> => ({
     signIn: closeAuthContainer,
     signUp: () => setAuthStep("signIn"),
     passwordResetEmail: () => setAuthStep("signIn"),
     passwordResetCode: () => setAuthStep("passwordResetEmail"),
-    newPassword: () => setAuthStep("passwordResetCode")
+    newPassword: () => setAuthStep("passwordResetCode"),
+    successfulSignIn: closeAuthContainer,
+    successfulSignUp: closeAuthContainer,
+    successfulPasswordChange: closeAuthContainer,
 });

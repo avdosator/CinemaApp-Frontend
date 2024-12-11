efeda19c6a620a809d18b661baf15d4aecd9ac92
Avdo Sator <avdo.sator@hotmail.com>
2024-12-11 09:06:54 +0100

    Edit Venue and Hall types

--- src/types/Hall.ts
+++ src/types/Hall.ts
@@ -1,5 +1,7 @@
+import { Venue } from "./Venue"
+
 export type Hall = {
     id: string,
-    name: string
-    // projections and venue will be added after updating Hall domain model on backend
+    name: string,
+    venue: Venue,
 }

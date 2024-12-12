91fffbe62af73acf028e15f5234e2cdaf8e34f40
Avdo Sator <avdo.sator@hotmail.com>
2024-12-12 20:34:58 +0100

    Add all elements to SeatGuide component and style it

--- src/components/seat-reservation-page/seat-guide/SeatGuide.tsx
+++ src/components/seat-reservation-page/seat-guide/SeatGuide.tsx
@@ -1,3 +1,4 @@
+import "./SeatGuide.css"
 import LoveSeat from "../seat/LoveSeat";
 import RegularSeat from "../seat/RegularSeat";
 import VipSeat from "../seat/VipSeat";
@@ -6,15 +7,35 @@ export default function SeatGuide() {
     return (
         <div className="seat-guide">
             <p className="font-lg-regular" style={{ color: "#1D2939", marginBottom: "0px" }}>Seat Guide</p>
-            <div className="seat-availability">
-                <RegularSeat number="XY" />
-                <RegularSeat number="XY" />
-                <RegularSeat number="XY" />
-            </div>
-            <div className="seat-prices">
-                <RegularSeat number="XY" />
-                <VipSeat number="XY" />
-                <LoveSeat number="XY" />
+            <div className="seat-guide-content">
+                <div className="seat-availability">
+                    <div className="seat-guide-group">
+                        <RegularSeat number="XY" />
+                        <span>Available</span>
+                    </div>
+                    <div className="seat-guide-group">
+                        <RegularSeat number="XY" classes="reserved-seat" />
+                        <span>Reserved</span>
+                    </div>
+                    <div className="seat-guide-group">
+                        <RegularSeat number="XY" classes="selected-seat" />
+                        <span>Selected</span>
+                    </div>
+                </div>
+                <div className="seat-prices">
+                <div className="seat-guide-group">
+                        <RegularSeat number="XY" />
+                        <span>Regular Seats(7 BAM)</span>
+                    </div>
+                    <div className="seat-guide-group">
+                    <VipSeat number="XY" />
+                        <span>VIP Seats (10 BAM)</span>
+                    </div>
+                    <div className="seat-guide-group">
+                    <LoveSeat number="XY" />
+                        <span>Love Seats(24 BAM)</span>
+                    </div>
+                </div>
             </div>
         </div>
     )

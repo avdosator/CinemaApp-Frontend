4d81953c390777fc5c46870498aa5e3210ffc46c
Avdo Sator <avdo.sator@hotmail.com>
2024-12-17 23:30:36 +0100

    Change session duration

--- src/components/reservation-ticket-page/ReservationTicketPage.tsx
+++ src/components/reservation-ticket-page/ReservationTicketPage.tsx
@@ -7,7 +7,7 @@ import ApiService from "../../service/ApiService";
 import { Seat } from "../../types/Seat";
 import BuyTicketPage from "./buy-ticket-page/BuyTicketPage";
 
-const SESSION_DURATION = 31100;
+const SESSION_DURATION = 300;
 
 export default function ReservationTicketPage() {
     const location = useLocation();

07551cfc9d05a0fbacafd6f115fd04e7ee1739ef
Avdo Sator <avdo.sator@hotmail.com>
2025-01-07 13:24:22 +0100

    Add movie tabs and basic style

--- src/components/admin-panel/movies-tab/MoviesPanel.tsx
+++ src/components/admin-panel/movies-tab/MoviesPanel.tsx
@@ -2,9 +2,14 @@ import "./MoviesPanel.css"
 
 export default function MoviesPanel() {
     return (
-        <div className="movies-tab">
-            <h6 className="font-heading-h6" style={{color: "#1D2939", marginBottom: "0px"}}>Movies</h6>
-            <div className="movie-tabs"></div>
+        <div className="movies-panel">
+            <h6 className="font-heading-h6" style={{color: "#1D2939", marginBottom: "10px"}}>Movies</h6>
+            <div className="movie-tabs-container">
+                <div className="font-lg-regular movie-tab">Drafts (0)</div>
+                <div className="font-lg-regular movie-tab">Currently Showing (0)</div>
+                <div className="font-lg-regular movie-tab">Upcoming (0)</div>
+                <div className="font-lg-regular movie-tab">Archived (0)</div>
+            </div>
             <div className="full-width-horizontal-line"></div>
         </div>
     )

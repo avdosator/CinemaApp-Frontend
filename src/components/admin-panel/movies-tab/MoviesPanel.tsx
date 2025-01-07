6485591a92faac4648b8d0e7a94822c32bb4cf34
Avdo Sator <avdo.sator@hotmail.com>
2025-01-07 15:00:59 +0100

    Add button and improve positioning

--- src/components/admin-panel/movies-tab/MoviesPanel.tsx
+++ src/components/admin-panel/movies-tab/MoviesPanel.tsx
@@ -3,12 +3,17 @@ import "./MoviesPanel.css"
 export default function MoviesPanel() {
     return (
         <div className="movies-panel">
-            <h6 className="font-heading-h6" style={{color: "#1D2939", marginBottom: "10px"}}>Movies</h6>
-            <div className="movie-tabs-container">
-                <div className="font-lg-regular movie-tab">Drafts (0)</div>
-                <div className="font-lg-regular movie-tab">Currently Showing (0)</div>
-                <div className="font-lg-regular movie-tab">Upcoming (0)</div>
-                <div className="font-lg-regular movie-tab">Archived (0)</div>
+            <div className="movie-panel-heading-container">
+                <div>
+                    <h6 className="font-heading-h6" style={{ color: "#1D2939", marginBottom: "10px" }}>Movies</h6>
+                    <div className="movie-tabs-container">
+                        <div className="font-lg-regular movie-tab">Drafts (0)</div>
+                        <div className="font-lg-regular movie-tab">Currently Showing (0)</div>
+                        <div className="font-lg-regular movie-tab">Upcoming (0)</div>
+                        <div className="font-lg-regular movie-tab">Archived (0)</div>
+                    </div>
+                </div>
+                <button className="add-movie-btn font-lg-semibold" id="addMovieBtn1">Add Movie</button>
             </div>
             <div className="full-width-horizontal-line"></div>
         </div>

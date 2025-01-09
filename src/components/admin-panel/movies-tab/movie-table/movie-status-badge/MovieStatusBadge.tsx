b60e9c560ec267ab7c5f2dcfdd2af421d72f799d
Avdo Sator <avdo.sator@hotmail.com>
2025-01-09 17:35:04 +0100

    Add rendering logic

--- src/components/admin-panel/movies-tab/movie-table/movie-status-badge/MovieStatusBadge.tsx
+++ src/components/admin-panel/movies-tab/movie-table/movie-status-badge/MovieStatusBadge.tsx
@@ -8,4 +8,32 @@ type MovieStatusBadgeProps = {
 
 export default function MovieStatusBadge({ statusType, daysRemaining, draftStep }: MovieStatusBadgeProps) {
 
+    const getStatusText = () => {
+        switch (statusType) {
+            case "currently-showing":
+                return `Ending in ${daysRemaining} days`;
+            case "upcoming":
+                return `Coming in ${daysRemaining} days`;
+            case "drafts":
+                return `Step ${draftStep}/3 completed`;
+            case "archived":
+                return `Ended`;
+        }
+    };
+
+    const getPillColor = () => {
+        if (statusType === "drafts") {
+            return draftStep === 3 ? "green-pill" : "yellow-pill";
+        }
+        if (statusType === "archived") {
+            return "red-pill";
+        }
+        return daysRemaining && daysRemaining > 7 ? "green-pill" : "yellow-pill";
+    };
+
+    return (
+        <span className={`font-medium-regular movie-status-badge ${getPillColor()}`}>
+            {getStatusText()}
+        </span>
+    );
 }

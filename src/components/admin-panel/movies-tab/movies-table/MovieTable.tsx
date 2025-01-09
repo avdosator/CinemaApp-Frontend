ea8d3bc79da8e35e4b793e5ad64b0f4969ef5519
Avdo Sator <avdo.sator@hotmail.com>
2025-01-09 10:52:04 +0100

    Add table structure

--- src/components/admin-panel/movies-tab/movies-table/MovieTable.tsx
+++ src/components/admin-panel/movies-tab/movies-table/MovieTable.tsx
@@ -6,8 +6,37 @@ type MovieTableProps = {
     showActions?: boolean;
 }
 
-export default function MovieTable({movies, showCheckbox = true, showActions = true}: MovieTableProps) {
+export default function MovieTable({ movies, showCheckbox = true, showActions = true }: MovieTableProps) {
     return (
-        
-    )
+        <div className="movie-table">
+            <table>
+                <thead>
+                    <tr>
+                        {showCheckbox && <th></th>}
+                        <th>Name</th>
+                        <th>Projection Date</th>
+                        <th>Venue</th>
+                        <th>Status</th>
+                        {showActions && <th>Action</th>}
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            {showCheckbox && <td><input type="checkbox" /></td>}
+                            <td>{movie.title}</td>
+                            <td>
+                                {movie.projections.map(projection =>
+                                    `${projection.startDate} - ${projection.endDate}`
+                                ).join(", ")}
+                            </td>
+                            <td>All Venues</td>
+                            <td>Status Here</td>
+                            {showActions && <td><button>...</button></td>}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }

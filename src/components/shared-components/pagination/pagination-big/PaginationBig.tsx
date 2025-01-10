c1f35dd93b58d32380ff12c79d3d6d7e0836e81a
Avdo Sator <avdo.sator@hotmail.com>
2025-01-10 11:24:44 +0100

    Add buttons

--- src/components/shared-components/pagination/pagination-big/PaginationBig.tsx
+++ src/components/shared-components/pagination/pagination-big/PaginationBig.tsx
@@ -9,14 +9,22 @@ export default function PaginationBig() {
                 Showing <span className="font-sm-semibold">10</span> out of <span className="font-sm-semibold">12</span> items.
             </div>
             <div className="pagination-big-control">
-                <FontAwesomeIcon icon={faAnglesLeft} width={16} height={16} />
-                <FontAwesomeIcon icon={faAngleLeft} width={16} height={16} />
+                <button>
+                    <FontAwesomeIcon icon={faAnglesLeft} width={16} height={16} />
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faAngleLeft} width={16} height={16} />
+                </button>
                 <span>Page</span>
                 <span className="pagination-big-current-page" >1</span>
                 <span>out of</span>
                 <span className="font-sm-semibold" style={{ color: "#101828" }}>2</span>
-                <FontAwesomeIcon icon={faAngleRight} width={16} height={16} />
-                <FontAwesomeIcon icon={faAnglesRight} width={16} height={16} />
+                <button>
+                    <FontAwesomeIcon icon={faAngleRight} width={16} height={16} />
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faAnglesRight} width={16} height={16} />
+                </button>
             </div>
             <div className="pagination-big-items-per-page">
                 <span>Display</span>

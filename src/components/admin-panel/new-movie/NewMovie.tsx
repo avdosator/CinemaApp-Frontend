996d169d9563cd2f882597ca6133292993b0a092
Avdo Sator <avdo.sator@hotmail.com>
2025-01-10 19:35:07 +0100

    Add heading elements to NewMovie component

--- src/components/admin-panel/new-movie/NewMovie.tsx
+++ src/components/admin-panel/new-movie/NewMovie.tsx
@@ -1,10 +1,18 @@
-import ProjectionsForm from "./projections-form/ProjectionsForm";
+import "./NewMovie.css"
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NewMovie() {
     return (
-        <div style={{display:"flex"}}>
-            {/* <SideBar /> */}
-            <ProjectionsForm />
+        <div className="add-movie-container">
+            <div className="add-movie-heading">
+                <h6 className="font-heading-h6">Add New Movie</h6>
+                <button className="add-movie-cancel-btn">
+                    <FontAwesomeIcon icon={faXmark} width={12} height={16} />
+                </button>
+            </div>
+            
+
         </div>
     )
 }

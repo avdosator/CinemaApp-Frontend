bc5d381fdd1963ffa2821dbdf6bae80a515fee78
Avdo Sator <avdo.sator@hotmail.com>
2025-01-14 13:15:14 +0100

    Edit placement of csv data

--- src/components/admin-panel/new-movie/details-form/writers-container/WritersContainer.tsx
+++ src/components/admin-panel/new-movie/details-form/writers-container/WritersContainer.tsx
@@ -3,7 +3,7 @@ export default function WritersContainer({ writersData }: { writersData: string[
     return (
         <div className="cast-writers-list">
             {writersData.map((writer, index) => (
-                <span className="font-md-semibold" key={index}>{writer}</span>
+                <span className="font-md-semibold" key={index} style={{ width: "calc(33.33% - 32px)" }}>{writer}</span>
             ))}
         </div>
     )

6ccf59071e2fad8f3656967469bb63978fb645ac
Avdo Sator <avdo.sator@hotmail.com>
2025-01-15 14:05:08 +0100

    Prevent rendering now for when previous is not complete

--- src/components/admin-panel/new-movie/NewMovie.tsx
+++ src/components/admin-panel/new-movie/NewMovie.tsx
@@ -12,7 +12,8 @@ import AddMovieStepIndicator from "./add-movie-step-indicator/AddMovieStepIndica
 
 export default function NewMovie() {
     const navigate = useNavigate();
-    const [currentStep, setCurrentStep] = useState<AddMovieFormStep>(1); // Every step is new form
+    const [currentStep, setCurrentStep] = useState<AddMovieFormStep>(1);
+    const [formNotFilledModal, setFormNotFilledModal] = useState<boolean>(false); // Every step is new form
 
     // GeneralForm state 
     let [generalFormData, setGeneralFormData] = useState<GeneralFormData>({
@@ -42,7 +43,9 @@ export default function NewMovie() {
     ]);
 
     const handleNextStep = () => {
-        setCurrentStep((prev) => Math.min(prev + 1, 3) as AddMovieFormStep);
+        if (validateCurrentStep()) {
+            setCurrentStep((prev) => Math.min(prev + 1, 3) as AddMovieFormStep);
+        }
     };
 
     const handlePreviousStep = () => {
@@ -60,8 +63,33 @@ export default function NewMovie() {
         return "inactive";
     };
 
+    const validateCurrentStep = () => {
+        if (
+            (currentStep === 1 && !isGeneralFormComplete) ||
+            (currentStep === 2 && !isDetailsFormComplete) ||
+            (currentStep === 3 && !isProjectionsFormComplete)
+        ) {
+            setFormNotFilledModal(true);
+            return false;
+        }
+        return true;
+    };
+
     return (
         <div className="add-movie-container">
+            {formNotFilledModal && (
+                <div className="session-expired-overlay">
+                    <div className="session-expired-modal">
+                        <h6 className="font-heading-h6" style={{ color: "#101828" }}>Form not completed</h6>
+                        <p className="font-md-regular" style={{ color: "#667085" }}> Please complete all required fields before proceeding.</p>
+                        <div className="session-expired-footer" style={{ gap: "8px" }}>
+                            <button className="font-sm-semibold new-bank-card-btn" style={{ width: "auto" }} onClick={() => setFormNotFilledModal(false)}>
+                                OK
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
             <div className="add-movie-heading">
                 <h6 className="font-heading-h6">Add New Movie</h6>
                 <button className="add-movie-cancel-btn" onClick={() => navigate("/admin")}>
@@ -79,6 +107,7 @@ export default function NewMovie() {
             {currentStep === 2 && (<DetailsForm detailsFormData={detailsFormData} setDetailsFormData={setDetailsFormData} />)}
             {currentStep === 3 && (<ProjectionsForm projectionsFormData={projectionsFormData} setProjectionsFormData={setProjectionsFormData} />)}
 
-            <ControlButtonGroup onNext={handleNextStep} onBack={handlePreviousStep} isBackDisabled={currentStep === 1} /></div>
+            <ControlButtonGroup onNext={handleNextStep} onBack={handlePreviousStep} isBackDisabled={currentStep === 1} />
+        </div>
     )
 }

72a38e699ad802e48a3e35db89aee99be4678207
Avdo Sator <avdo.sator@hotmail.com>
2025-01-25 16:27:27 +0100

    Add logic for fetching venues

--- src/components/admin-panel/venues-tab/VenuesPanel.tsx
+++ src/components/admin-panel/venues-tab/VenuesPanel.tsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import TertiaryButton from "../../shared-components/buttons/TertiaryButton";
+import AdminPanelVenueList from "./venue-list/AdminPanelVenueList";
 import "./VenuesPanel.css"
+import { Venue } from "../../../types/Venue";
+import ApiService from "../../../service/ApiService";
+import { PageResponse } from "../../../types/PageResponse";
+
+const PAGE_SIZE = 6;
 
 export default function VenuesPanel() {
+    const [venues, setVenues] = useState<Venue[]>([]);
+    let [page, setPage] = useState<number>(0);
+    let [isLastPage, setIsLastPage] = useState<boolean>(false);
+
+    useEffect(() => {
+        fetchVenues(0);
+    }, [])
+
+    const fetchVenues = (pageNumber: number) => {
+        ApiService.get<PageResponse<Venue>>("/venues", { page: pageNumber, size: PAGE_SIZE } )
+            .then(response => {
+                setVenues(prevVenues =>
+                    pageNumber === 0 ? response.content : [...prevVenues, ...response.content]
+                );
+
+                setIsLastPage(response.last);
+            })
+            .catch(error => console.log(error));
+    }
+
+    const handleLoadMore = () => {
+        const nextPage = page + 1;
+        setPage(nextPage);
+        fetchVenues(nextPage);
+    };
+
     return (
         <div className="venues-panel">
             <div className="venues-panel-heading-container">
@@ -10,6 +44,12 @@ export default function VenuesPanel() {
                 </div>
                 <div className="full-width-horizontal-line"></div>
             </div>
+            <AdminPanelVenueList venues={venues} />
+            {!isLastPage && venues.length > 0 && (
+                <div className="load-more-btn">
+                    <TertiaryButton label="Load More" size="large" onClick={handleLoadMore} />
+                </div>
+            )}
         </div>
 
     );

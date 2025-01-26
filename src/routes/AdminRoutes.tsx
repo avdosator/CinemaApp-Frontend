78d3cd9d8e4dcadb3ddc7c3e50421b773242f006
Avdo Sator <avdo.sator@hotmail.com>
2025-01-26 16:58:45 +0100

    Add routing logic

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import MoviesPanel from "../components/admin-panel/movies-tab/MoviesPanel";
 import NewMovie from "../components/admin-panel/new-movie/NewMovie";
 import VenuesPanel from "../components/admin-panel/venues-tab/VenuesPanel";
+import VenueForm from "../components/admin-panel/venues-tab/venue-form/VenueForm";
 
 export default function AdminRoutes() {
     return (
@@ -14,6 +15,9 @@ export default function AdminRoutes() {
             <Route path="movies/archived" element={<MoviesPanel />} />
             <Route path="movies/new-movie" element={<NewMovie />} />
             <Route path="venues" element={<VenuesPanel />} />
+            <Route path="venues/:id" element={<VenueForm mode="view" />} />
+            <Route path="venues/new-venue" element={<VenueForm mode="add" />} />
+            <Route path="venues/:id/edit" element={<VenueForm mode="edit" />} />
         </Routes>
     )
 }

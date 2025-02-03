211dcc46521e732423823ff274f112ddbc7ba340
Avdo Sator <avdo.sator@hotmail.com>
2025-02-03 20:26:32 +0100

    Update type/model fields

--- src/types/ProjectionInstance.ts
+++ src/types/ProjectionInstance.ts
@@ -1,11 +1,9 @@
-import { Projection } from "./Projection"
 import { SeatReservation } from "./SeatReservation"
 
 export type ProjectionInstance = {
     id: string,
-    projection: Projection,
+    //projection: Projection,
     date: Date,
     time: string,
     seatReservations: SeatReservation[]
-    seatsStatus: Record<string, string>
 }

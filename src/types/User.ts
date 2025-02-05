8ac614708c3c1b43727c96685b633cdd0a70cfd8
Avdo Sator <avdo.sator@hotmail.com>
2025-02-05 20:25:06 +0100

    Add properties

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,11 @@
+import { City } from "./City";
+
 export type User = {
     id: string,
+    firstName: string,
+    lastName: string,
     email: string,
+    phone: string,
+    city: City,
     role: string,
-    // add other props later
 }

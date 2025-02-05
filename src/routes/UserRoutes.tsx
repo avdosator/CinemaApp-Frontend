6cfb0e7b14c6dad48d55e82dac394a1c386b2ed3
Avdo Sator <avdo.sator@hotmail.com>
2025-02-05 22:17:33 +0100

    Add route

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import PersonalInformation from "../components/user-profile-page/personal-information/PersonalInformation";
+import EditProfile from "../components/user-profile-page/edit-profile/EditProfile";
 
 export default function UserRoutes() {
     return (
@@ -7,6 +8,7 @@ export default function UserRoutes() {
             {/* Redirect to personal information when /user is accessed */}
             <Route path="" element={<Navigate to="/user/personal-information" replace />} />
             <Route path="personal-information" element={<PersonalInformation />} />
+            <Route path="edit-profile" element={<EditProfile />} />
         </Routes>
     );
 }

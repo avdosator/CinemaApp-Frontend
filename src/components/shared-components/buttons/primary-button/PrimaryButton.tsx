614faaa0a9e6c4ab221b18fb526aa6b8a566f511
Avdo Sator <avdo.sator@hotmail.com>
2025-03-04 21:28:57 +0100

    Add more props

--- src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
+++ src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
@@ -1,12 +1,20 @@
 import "./PrimaryButton.css"
 
 type PrimaryButtonProps = {
-    label: string
+    label: string,
+    onClick: () => void,
+    isDisabled: boolean,
+    isFullWidth: boolean
 }
 
-export default function PrimaryButton({label} : PrimaryButtonProps) {
+export default function PrimaryButton({label, onClick, isDisabled, isFullWidth} : PrimaryButtonProps) {
     return (
-        <button className="font-lg-semibold">
+        <button 
+        className="primary-button font-lg-semibold" 
+        onClick={onClick} 
+        disabled={isDisabled}
+        style={{ width: isFullWidth ? "100%" : "auto" }}
+        >
             {label}
         </button>
     );

8a422c458f79264f3688c781537107cf9bb812d2
Avdo Sator <avdo.sator@hotmail.com>
2025-03-05 19:43:32 +0100

    Add new prop

--- src/components/shared-components/buttons/TertiaryButton.tsx
+++ src/components/shared-components/buttons/TertiaryButton.tsx
@@ -4,14 +4,15 @@ type TertiaryButtonProps = {
     label: string,
     size: string,
     onClick?: () => void,
-    color?: string
+    color?: string,
+    isDisabled?: boolean
 }
 
-export default function TertiaryButton({label, size, onClick, color}: TertiaryButtonProps) {
+export default function TertiaryButton({ label, size, onClick, color, isDisabled }: TertiaryButtonProps) {
     const btnSize = size === "large" ? "tertiary-btn-lg font-lg-underline-semibold" : "tertiary-btn-sm font-sm-underline-semibold";
-    const style = color ? {color: color} : {}
+    const style = color ? { color: color } : {}
     return (
-        <button className={`tertiary-btn ${btnSize}`} onClick={onClick} style={style} type="button">
+        <button className={`tertiary-btn ${btnSize}`} onClick={onClick} style={style} type="button" disabled={isDisabled}>
             {label}
         </button>
     )

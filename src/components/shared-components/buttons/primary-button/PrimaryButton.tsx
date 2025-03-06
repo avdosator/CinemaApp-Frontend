28738b6c9d5da89519e6d8395c6bb4e556bfd796
Avdo Sator <avdo.sator@hotmail.com>
2025-03-06 16:39:59 +0100

    Add default value for optional props

--- src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
+++ src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
@@ -4,12 +4,12 @@ import "./PrimaryButton.css"
 type PrimaryButtonProps = {
     label: string,
     onClick?: () => void,
-    isDisabled: boolean,
-    isFullWidth: boolean,
+    isDisabled?: boolean,
+    isFullWidth?: boolean,
     style?: CSSProperties
 }
 
-export default function PrimaryButton({ label, onClick, isDisabled, isFullWidth, style }: PrimaryButtonProps) {
+export default function PrimaryButton({ label, onClick, isDisabled = false, isFullWidth = false, style }: PrimaryButtonProps) {
     const combinedStyle: CSSProperties = { ...style, width: isFullWidth ? "100%" : "auto" };
     return (
         <button

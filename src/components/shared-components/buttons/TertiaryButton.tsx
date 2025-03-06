940ce0058de75c16bcff3aedbb9d5b29d4649c75
Avdo Sator <avdo.sator@hotmail.com>
2025-03-06 16:21:08 +0100

    Add more props to TertiaryButton

--- src/components/shared-components/buttons/TertiaryButton.tsx
+++ src/components/shared-components/buttons/TertiaryButton.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties, ReactNode } from "react";
 import "./TertiaryButton.css"
 
 type TertiaryButtonProps = {
@@ -5,14 +6,18 @@ type TertiaryButtonProps = {
     size: string,
     onClick?: () => void,
     color?: string,
-    isDisabled?: boolean
+    isDisabled?: boolean,
+    icon?: ReactNode,
+    style?: CSSProperties
 }
 
-export default function TertiaryButton({ label, size, onClick, color, isDisabled }: TertiaryButtonProps) {
+export default function TertiaryButton({ label, size, onClick, color, isDisabled = false, icon, style }: TertiaryButtonProps) {
     const btnSize = size === "large" ? "tertiary-btn-lg font-lg-underline-semibold" : "tertiary-btn-sm font-sm-underline-semibold";
-    const style = color ? { color: color } : {}
+    const combinedStyle: CSSProperties = { color, ...style }; // Merge color and custom styles
+
     return (
-        <button className={`tertiary-btn ${btnSize}`} onClick={onClick} style={style} type="button" disabled={isDisabled}>
+        <button className={`tertiary-btn ${btnSize}`} onClick={onClick} style={combinedStyle} type="button" disabled={isDisabled}>
+            {icon && (<span>{icon}</span>)}
             {label}
         </button>
     )

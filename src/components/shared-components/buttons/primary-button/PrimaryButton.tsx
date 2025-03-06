d3f3a2faacb4e3fda3bcaa05ff4dde38659740a7
Avdo Sator <avdo.sator@hotmail.com>
2025-03-06 16:34:38 +0100

    Add style prop

--- src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
+++ src/components/shared-components/buttons/primary-button/PrimaryButton.tsx
@@ -1,19 +1,22 @@
+import { CSSProperties } from "react";
 import "./PrimaryButton.css"
 
 type PrimaryButtonProps = {
     label: string,
     onClick?: () => void,
     isDisabled: boolean,
-    isFullWidth: boolean
+    isFullWidth: boolean,
+    style?: CSSProperties
 }
 
-export default function PrimaryButton({label, onClick, isDisabled, isFullWidth} : PrimaryButtonProps) {
+export default function PrimaryButton({ label, onClick, isDisabled, isFullWidth, style }: PrimaryButtonProps) {
+    const combinedStyle: CSSProperties = { ...style, width: isFullWidth ? "100%" : "auto" };
     return (
-        <button 
-        className="primary-button font-lg-semibold" 
-        onClick={onClick} 
-        disabled={isDisabled}
-        style={{ width: isFullWidth ? "100%" : "auto" }}
+        <button
+            className="primary-button font-lg-semibold"
+            onClick={onClick}
+            disabled={isDisabled}
+            style={combinedStyle}
         >
             {label}
         </button>

111493408df82052aa444e9d6c786bf75f15e366
Avdo Sator <avdo.sator@hotmail.com>
2025-03-12 20:16:27 +0100

    Use SecondaryButton in EditProfile component

--- src/components/user-profile-page/edit-profile/edit-profile-control-button-group/EditProfileControlButtonGroup.tsx
+++ src/components/user-profile-page/edit-profile/edit-profile-control-button-group/EditProfileControlButtonGroup.tsx
@@ -2,6 +2,7 @@ import "./EditProfileControlButtonGroup.css"
 import TertiaryButton from "../../../shared-components/buttons/tertiary-button/TertiaryButton";
 import { useNavigate } from "react-router-dom";
 import PrimaryButton from "../../../shared-components/buttons/primary-button/PrimaryButton";
+import SecondaryButton from "../../../shared-components/buttons/secondary-button/SecondaryButton";
 
 type EditProfileControlButtonGroupProps = {
     handleUpdateProfile: () => void,
@@ -14,7 +15,7 @@ export default function EditProfileControlButtonGroup({ handleUpdateProfile, dea
         <div className="edit-profile-control-button-group">
             <TertiaryButton label="Deactivate My Account" size="large" onClick={deactivateProfile} />
             <div className="edit-profile-right-button-group font-lg-semibold">
-                <button onClick={() => navigate("/user/personal-information")}>Cancel</button>
+                <SecondaryButton label="Cancel" onClick={() => navigate("/user/personal-information")} />
                 <PrimaryButton label="Save Changes" onClick={handleUpdateProfile} />
             </div>
         </div>
